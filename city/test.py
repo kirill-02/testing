33730b946bc6de8e6629b93a1875949aac95f07e
kirill-02 <136984088+kirill-02@users.noreply.github.com>
2026-04-21 22:55:14 +0400

    update

--- city/test.py
+++ city/test.py
@@ -3,44 +3,44 @@ import uuid
 from datetime import datetime
 
 
-class NameManager:
+class StreetManager:
     def __init__(self):
-        self.names = {}
+        self.streets = {}
         self.add("Удмуртская")
         self.add("Ленина")
 
-    def add(self, names):
-        if not names or any(s['name'] == names for s in self.names.values()):
+    def add(self, name):
+        if not name or any(s['name'] == name for s in self.streets.values()):
             return False
         id = uuid.uuid4().hex
-        self.names[id] = {
+        self.streets[id] = {
             'id': id,
-            'name': names,
+            'name': name,
             'created_at': datetime.now(),
             'updated_at': datetime.now()
         }
         return True
 
-    def update(self, id, names):
-        if id not in self.names or not names:
+    def update(self, id, name):
+        if id not in self.streets or not name:
             return False
-        self.names[id]['name'] = names
-        self.names[id]['updated_at'] = datetime.now()
+        self.streets[id]['name'] = name
+        self.streets[id]['updated_at'] = datetime.now()
         return True
 
     def delete(self, id):
-        if id not in self.names:
+        if id not in self.streets:
             return False
-        del self.names[id]
+        del self.streets[id]
         return True
 
     def get_all(self):
-        return list(self.names.values())
+        return list(self.streets.values())
 
 
-class TestNames(unittest.TestCase):
+class TestStreets(unittest.TestCase):
     def setUp(self):
-        self.m = NameManager()
+        self.m = StreetManager()
 
     def test_add(self):
         self.assertTrue(self.m.add('Пушкинская'))
@@ -50,35 +50,29 @@ class TestNames(unittest.TestCase):
         self.assertFalse(self.m.add('Удмуртская'))
 
     def test_update(self):
-        names = self.m.get_all()
-        self.assertTrue(self.m.update(names[0]['id'], 'Удмуртская улица'))
+        streets = self.m.get_all()
+        self.assertTrue(self.m.update(streets[0]['id'], 'Удмуртская улица'))
         self.assertEqual('Удмуртская улица', self.m.get_all()[0]['name'])
 
     def test_update_nonexistent(self):
         self.assertFalse(self.m.update('ххх', 'Тест'))
 
     def test_delete(self):
-        names = self.m.get_all()
-        self.assertTrue(self.m.delete(names[1]['id']))
+        streets = self.m.get_all()
+        self.assertTrue(self.m.delete(streets[1]['id']))
         self.assertEqual(1, len(self.m.get_all()))
 
     def test_delete_nonexistent(self):
         self.assertFalse(self.m.delete('ххх'))
 
+
     def test_get_all(self):
-        names = self.m.get_all()
-        self.assertEqual(2, len(names))
-        names = [s['name'] for s in names]
+        streets = self.m.get_all()
+        self.assertEqual(2, len(streets))
+        names = [s['name'] for s in streets]
         self.assertIn('Удмуртская', names)
         self.assertIn('Ленина', names)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
-
-
